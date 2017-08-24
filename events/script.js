5f48d205a64750f89a8f41033cd8c86d67b090a3
Pierre Cavalet <pcavalet@kaliop.com>
2017-08-24 15:26:48 +0200

    correction events

--- events/script.js
+++ events/script.js
@@ -1,6 +1,13 @@
 // ex 1
 // TODO: Add a click event listener and log in the console 'clicked'
+var clickMe = document.querySelector('#click-me');
 
+var clickHandler =  function (event) {
+  console.log('clicked');
+};
+
+clickMe.addEventListener('click', clickHandler);
 
 // ex 2
 // TODO: Remove the event listener (not the code, remove it from the element)
+clickMe.removeEventListener('click', clickHandler);
