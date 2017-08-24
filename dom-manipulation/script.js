c5b7689a2d4a98dab80a1e2fe95c416559c7c06b
Pierre Cavalet <pcavalet@kaliop.com>
2017-08-24 11:47:49 +0200

    retrieve input value

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -8,3 +8,10 @@
 
 // ex 3
 // TODO: Transform Jack in green using the jack-list list
+
+
+// ex 4
+var displayButton = document.querySelector('#ex4-display')
+displayButton.addEventListener('click', function (event) {
+  // TODO: display the value of the input
+})
