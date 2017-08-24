d4a2aa8b2728e871383cf256e09fc8f435f7f97f
Pierre Cavalet <pcavalet@kaliop.com>
2017-08-24 11:32:08 +0200

    dom manipulation correction

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,10 +1,18 @@
 // ex 1
 // TODO: Transform the following text in red
-
+document.querySelector('#i-should-be-red').style.color = "red";
 
 // ex 2
 // TODO: Transform the following texts in blue
+var shouldBeBlue = document.querySelectorAll('.we-should-be-blue');
+shouldBeBlue.forEach(element => {
+  element.style.color = "blue";
+});
 
-
-// ex 1
+// ex 3
 // TODO: Transform Jack in green using the jack-list list
+var goodList = document.querySelector('#good-list');
+var shouldBeGreen = goodList.querySelectorAll('li');
+shouldBeGreen.forEach(element => {
+  element.style.color = "green";
+});
