1eeb6a1d6c05b32ffbabf7c09aab82c76f567066
Pierre Cavalet <pcavalet@kaliop.com>
2017-08-24 11:22:30 +0200

    dom manipulation

---
+++ dom-manipulation/script.js
@@ -0,0 +1,10 @@
+// ex 1
+// TODO: Transform the following text in red
+
+
+// ex 2
+// TODO: Transform the following texts in blue
+
+
+// ex 1
+// TODO: Transform Jack in green using the jack-list list
