4f94366e13fbcee3aac445d3c3d9b952704e5b20
Pierre Cavalet <pcavalet@kaliop.com>
2017-08-24 14:25:38 +0200

    add corrections

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -24,32 +24,38 @@ displayButton4.addEventListener('click', function (event) {
   // TODO: display the value of the input
   var value = document.querySelector('#ex4-input').value;
   console.log(value);
-})
+});
 
 // ex 5
 // TODO: Add a 'first' class to the first span
-
+var first = document.querySelector('#first')
+first.classList.add('first');
 
 // ex 6
 // TODO: Remove the 'second' class to the second span
-
+var second = document.querySelector('#second')
+second.classList.remove('second');
 
 // ex 7
 var toggleButton7 = document.querySelector('#ex7-toggle')
 toggleButton7.addEventListener('click', function (event) {
   // TODO: display the value of the input
-})
+  document.querySelector('#third').classList.toggle('third');
+});
 
 // ex 8
 // TODO: Remove the paragraph 'remove-me'
-
+var removeMe = document.querySelector('#remove-me');
+removeMe.parentNode.removeChild(removeMe);
 
 // ex 9
 // TODO: Display the span value in the console when you click on the button
 var displayButton9 = document.querySelector('#ex9-display')
 displayButton9.addEventListener('click', function (event) {
   // TODO: display the value of the span
-})
+  var getText = document.querySelector('#get-text');
+  console.log(getText.textContent)
+});
 
 
 // ex 10
@@ -57,8 +63,16 @@ displayButton9.addEventListener('click', function (event) {
 var displayButton10 = document.querySelector('#ex10-display')
 displayButton10.addEventListener('click', function (event) {
   // TODO: display the value of the span
-})
+  var getText = document.querySelector('#ex10');
+  console.log(getText.innerHTML)
+});
 
 
 // ex 11
 // TODO: Create a paragraph element and insert it in the div
+var paragraph = document.createElement('p');
+var node = document.createTextNode('a new paragraph');
+paragraph.appendChild(node);
+
+var container = document.getElementById('paragraph-container');
+container.appendChild(paragraph);
