b449f4ee9dffead286b2198b514d3e4aa429b6fb
Pierre Cavalet <pcavalet@kaliop.com>
2017-08-24 16:58:10 +0200

    add array map and reduce exercices

---
+++ array-function/map.js
@@ -0,0 +1,65 @@
+console.log('Exercice 1: Array map');
+
+// Exercice 1.1
+console.log('Exercice 1.1');
+var input1 = [0, 1, 2, 3];
+var result1;
+
+// TODO: from the input1, create an array result1 that contains
+// the same value + 1 using the map function
+// ex: [0, 1, 2, 3] -> [1, 2, 3, 4]
+
+console.log('input', input1);
+console.log('result', result1);
+
+// Exercice 1.2
+
+console.log('Exercice 1.2');
+var input2 = ["a", "ab", "abc", "abcd"];
+var result2;
+
+// TODO: from the input2, create an array result2 that contains
+// the string length of each mapped element
+// ex: ["a", "ab", "abc", "abcd"] -> [1, 2, 3, 4]
+
+console.log('input', input2);
+console.log('result', result2);
+
+// Exercice 1.3
+
+console.log('Exercice 1.3');
+var input3 = [
+  {
+    firstName: 'Jack',
+    lastName: 'Sparrow'
+  },
+  {
+    firstName: 'Davy',
+    lastName: 'Jones'
+  },
+  {
+    firstName: 'Hector',
+    lastName: 'Barbossa'
+  }
+];
+var result3;
+
+// TODO: from the input3, create an array result3 that contains
+// the only the last name of each character
+// ex: [
+//   {
+//     firstName: 'Jack',
+//     lastName: 'Sparrow'
+//   },
+//   {
+//     firstName: 'Davy',
+//     lastName: 'Jones'
+//   },
+//   {
+//     firstName: 'Hector',
+//     lastName: 'Barbossa'
+//   }
+// ] -> ['Sparrow', 'Jones', 'Barbossa']
+
+console.log('input', input3);
+console.log('result', result3);
