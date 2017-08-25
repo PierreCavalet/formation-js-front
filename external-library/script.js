a7c381042bbf6fb73529e9a920eb1469762cc8ec
Pierre Cavalet <pcavalet@kaliop.com>
2017-08-25 11:31:27 +0200

    external library

---
+++ external-library/script.js
@@ -0,0 +1,8 @@
+// ex 1
+// TODO: Import axios HTTP client with the unpkg CDN
+
+// ex 2
+// TODO: Fill the post1 title and body with an HTTP request on the JSONPlaceholder API
+
+// ex 3
+// TODO: Send a new post to the server
