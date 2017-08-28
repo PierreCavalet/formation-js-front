581fffd1d935f85c14ee135a96a652617d90a8ef
Pierre Cavalet <pcavalet@kaliop.com>
2017-08-28 14:29:27 +0200

    data structure correction

--- data-structure/array.js
+++ data-structure/array.js
@@ -1,12 +1,18 @@
 // TODO: 1) create an array named 'colors' with the following elements:
 // "red", "blue", "yellow"
+var colors = ['red', 'blue', 'yellow'];
 
 // TODO: 2) access the second element (Array indexes start at 0 !)
+console.log(colors[1]);
 
 // TODO: 3) add a new element 'green' at the end
+colors.push('green');
 
 // TODO: 4) remove the last element (green)
+colors.pop();
 
 // TODO: 5) remove the first element (red)
+colors.shift();
 
 // TODO: 6) add a new element 'purple' at the beginning
+colors.unshift('purlple');
--- data-structure/object.js
+++ data-structure/object.js
@@ -1,17 +1,34 @@
 // TODO: 1) create an object named 'cat' with 'name', 'legs' and 'tails' properties
+var cat = {
+  name: 'felix',
+  legs: 4,
+  tails: 1
+};
 
 // TODO: 2) access the name property with the dot operator (.)
+console.log(cat.name);
 
 // TODO: 3) access the legs property with the bracket notation ([])
+console.log(cat['legs']);
 
 // TODO: 4) access the legs property with the bracket notation and a variable
+var legs = 'legs';
+console.log(cat[legs]);
 
 // TODO: 5) modify the legs property
+cat.legs = 3;
 
 // TODO: 6) add a new head property
+cat.head = 1;
 
 // TODO: 7) remove the tails property
+delete cat.tails;
 
 // TODO: 8) check if the head property exist in the cat object
+console.log(cat.hasOwnProperty('head'));
 
 // TODO: 9) nest the cat inside an other object (for example, John, who owns the cat)
+var john = {
+  name: 'John',
+  cat: cat
+}
