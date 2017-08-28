316235fa1286926b62fd519f8b8629f37bb911a2
Pierre Cavalet <pcavalet@kaliop.com>
2017-08-28 14:20:02 +0200

    data structure

---
+++ data-structure/array.js
@@ -0,0 +1,12 @@
+// TODO: 1) create an array named 'colors' with the following elements:
+// "red", "blue", "yellow"
+
+// TODO: 2) access the second element (Array indexes start at 0 !)
+
+// TODO: 3) add a new element 'green' at the end
+
+// TODO: 4) remove the last element (green)
+
+// TODO: 5) remove the first element (red)
+
+// TODO: 6) add a new element 'purple' at the beginning
---
+++ data-structure/object.js
@@ -0,0 +1,17 @@
+// TODO: 1) create an object named 'cat' with 'name', 'legs' and 'tails' properties
+
+// TODO: 2) access the name property with the dot operator (.)
+
+// TODO: 3) access the legs property with the bracket notation ([])
+
+// TODO: 4) access the legs property with the bracket notation and a variable
+
+// TODO: 5) modify the legs property
+
+// TODO: 6) add a new head property
+
+// TODO: 7) remove the tails property
+
+// TODO: 8) check if the head property exist in the cat object
+
+// TODO: 9) nest the cat inside an other object (for example, John, who owns the cat)
